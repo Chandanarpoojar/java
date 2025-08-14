15108aea1c596daae2f5b15c4f4b5d0d402335cc
chandanarpoojar <chandanarpoojarx-workz@outlook.com>
2025-08-14 19:25:35 +0530

    Temparature.java

---
+++ if condition/Temparature.java
@@ -0,0 +1,192 @@
+class Temparature
+{
+    public static double place(int celsius)
+	{
+	double place;
+	System.out.println("measure the average temparature of a country");
+	if(celsius == 1)
+	{
+	System.out.println("temparature in Davanagere");
+	celsius = 69;
+     return celsius;
+	 } 
+	 if(celsius == 2)
+	{
+	System.out.println("temperture in Ballari");
+	celsius = 78;
+     return celsius;
+	 } 
+	
+	if(celsius == 3)
+	{
+	System.out.println("temperture in Belagavi");
+	celsius = 70;
+     return celsius;
+	 } 
+	
+	if(celsius == 4)
+	{
+	System.out.println("temperture in Bidar");
+	celsius = 73;
+     return celsius;
+	 } 
+	if(celsius == 5)
+	{
+	System.out.println("temperture in Kalaburgi");
+	celsius = 74;
+     return celsius;
+	 } 
+	
+	if(celsius == 6)
+	{
+	System.out.println("temperture in Raichur");
+	celsius = 77;
+     return celsius;
+	 } 
+	
+	if(celsius == 7)
+	{
+	System.out.println("temperture in Chitradurga");
+	celsius = 76;
+     return celsius;
+	 } 
+	
+	if(celsius == 8)
+	{
+	System.out.println("temperture in Mysore");
+	celsius = 78;
+     return celsius;
+	 } 
+	
+	if(celsius == 9)
+	{
+	System.out.println("temperture in Mangalore");
+	celsius = 77;
+     return celsius;
+	 } 
+	if(celsius == 10)
+	{
+	System.out.println("temperture in Shimoga");
+	celsius = 79;
+     return celsius;
+	 } 
+	
+	if(celsius == 11)
+	{
+	System.out.println("temperture in Tumkur");
+	celsius = 72;
+     return celsius;
+	 } 
+	if(celsius == 12)
+	{
+	System.out.println("temperture in Bengaluru");
+	celsius = 77;
+     return celsius;
+	 } 
+	
+	if(celsius == 13)
+	{
+	System.out.println("temperture in Sringeri");
+	celsius = 75;
+     return celsius;
+	 } 
+	if(celsius == 14)
+	{
+	System.out.println("temperture in Haveri");
+	celsius = 77;
+     return celsius;
+	 } 
+	if(celsius == 15)
+	{
+	System.out.println("temperture in Hubballi");
+	celsius = 77;
+     return celsius;
+	 } 
+	if(celsius == 16)
+	{
+	System.out.println("temperture in Kadur");
+	celsius = 75;
+     return celsius;
+	 } 
+	if(celsius == 17)
+	{
+	System.out.println("temperture in Kundapura");
+	celsius = 75;
+     return celsius;
+	 } 
+	if(celsius == 18)
+	{
+	System.out.println("temperture in Tarikere");
+	celsius = 75;
+     return celsius;
+	 } 
+	if(celsius == 19)
+	{
+	System.out.println("temperture in Udupi");
+	celsius = 75;
+     return celsius;
+	 } 
+	if(celsius == 20)
+	{
+	System.out.println("temperture in Dharwad");
+	celsius = 73;
+     return celsius;
+	 } 
+	if(celsius == 21)
+	{
+	System.out.println("temperture in Mudigere");
+	celsius = 75;
+     return celsius;
+	 } 
+	if(celsius == 22)
+	{
+	System.out.println("temperture in Vijayapura");
+	celsius = 79;
+     return celsius;
+	 } 
+	if(celsius == 23)
+	{
+	System.out.println("temperture in Bhadravathi");
+	celsius = 72;
+     return celsius;
+	 } 
+	if(celsius == 24)
+	{
+	System.out.println("temperture in Bijapur");
+	celsius = 74;
+     return celsius;
+	 } 
+	if(celsius == 25)
+	{
+	System.out.println("temperture in Chikmagaluru");
+	celsius = 76;
+     return celsius;
+	 } 
+	 if(celsius == 26)
+	{
+	System.out.println("temperture in Tumkur");
+	celsius = 78;
+     return celsius;
+	 } 
+	if(celsius == 27)
+	{
+	System.out.println("temperture in Devanahalli");
+	celsius = 73;
+     return celsius;
+	 } 
+	if(celsius == 28)
+	{
+	System.out.println("temperture in Hubli-Dharwad");
+	celsius = 73;
+     return celsius;
+	 } 
+	if(celsius == 29)
+	{
+	System.out.println("temperture in Bengaluru Urban");
+	celsius = 73;
+     return celsius;
+	 } 
+	return 0;
+	 } 
+	 
+	 }
